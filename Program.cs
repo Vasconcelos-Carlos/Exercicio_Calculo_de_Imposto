d549f49607f35f09f5c22d28ffbf8c9e146550e5
Carlos Alberto <carlos98.camv@gmail.com>
2020-01-27 16:39:50 -0300

    criando repositorio

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Calculo_de_Imposto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
